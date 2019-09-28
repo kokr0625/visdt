468901076f3ff19cd3f80c2d7909ce606c86bbfd
Glitch (hello-express) <none>
2019-09-28 12:26:13 +0000

    💳💄 Checkpoint ./views/index.html:520522/25553 ./public/script.js:520522/4826

---
+++ public/script.js
@@ -0,0 +1,102 @@
+var Vue = Vue
+var VueRouter = VueRouter
+var Vuetify = Vuetify
+
+Vue.component('navigation', {
+  template: `
+<div id="app">
+      
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+<v-list-item 
+v-for="it in menus" :key="it.title" :to="it.to" @click="">
+<v-list-item-action>
+            <v-icon>{{it.title}}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{it.to}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+</vlist>
+
+
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+     <v-footer app dark>
+        <router-view></router-view>
+      
+      </v-footer>
+
+    
+</div>
+
+  `,
+
+  data() {
+    return {
+      menus: [{
+          title: 'dashboard',
+          to: '/',
+        },
+        {
+          title: 'settings',
+          to: '/02',
+        },
+      ],
+      menuActive: false,
+      drawer: null,
+
+    }
+  },
+
+  methods: {
+    toggleMenu() {
+      this.menuActive = !this.menuActive
+    }
+  }
+})
+
+const Home = {
+  template: '<p>This is the homepage.</p>'
+}
+const Two = {
+  template: '<p>This is the second page.</p>'
+}
+
+const routes = [{
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/02',
+    component: Two
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+var app = new Vue({
+  router,
+  el: '#app',
+  vuetify: new Vuetify({
+    theme: {
+      dark: true
+    },
+  }),
+  data: {
+    //
+  }
+})
