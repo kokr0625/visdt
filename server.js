9e3dcb5b7d157d292da64b630d89e8d22fa6a2bc
Glitch (hello-express) <none>
2019-09-29 15:15:39 +0000

    🎎🐣 Checkpoint ./package.json:520522/1283 ./server.js:520522/40

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 const express = require('express');
 const app = express();
+var mongoose = require('mongoose');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
