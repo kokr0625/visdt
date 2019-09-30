e1ce3fc9a985619612fca98628909efe305c4cde
Glitch (hello-express) <none>
2019-09-30 16:26:34 +0000

    🦃👻 Checkpoint ./vue.config.js:520522/82 ./package.json:520522/14 ./src/App.vue:520522/48

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <v-app-bar app>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <v-toolbar-title class="headline text-uppercase">
         <span>Vuetify</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,6 @@ module.exports = {
   devServer: {
     disableHostCheck: true,
     host: "0.0.0.0",
-    historyApiFallback: true,
-    noInfo: true,
-    overlay: true
-  },
+    port: "8080"
+   },
 };
