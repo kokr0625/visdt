185f46d64f2356144a80ec1d519990f91399f1a3
Glitch (hello-express) <none>
2019-09-30 13:57:54 +0000

    🐯👝 Checkpoint ./package.json:520522/11192 ./webpack.config.js:520522/423

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,24 @@ module.exports = {
     rules: [
       { test: /\.js$/, use: 'babel-loader' },
       { test: /\.vue$/, use: 'vue-loader' },
-      { test: /\.css$/, use: ['vue-style-loader', 'css-loader'] },
+      //{ test: /\.css$/, use: ['vue-style-loader', 'css-loader'] }
       
+    {
+      test: /\.s(c|a)ss$/,
+      use: [
+        'vue-style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          // Requires sass-loader@^7.0.0
+          options: {
+            implementation: require('sass'),
+            fiber: require('fibers')
+          }
+        }
+      ]
+    }
+  
     ]
   },
   devServer: {
